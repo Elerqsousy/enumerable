cf33189f3852da3671f84716806bd15da304133a
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-12-13 20:16:32 +0000

    create module for methods

---
+++ MyEnumerable.rb
@@ -0,0 +1,14 @@
+module MyEnumerable
+    def all?
+      result = true
+      each {|e| result = false unless yield e }  
+    result
+    end
+
+    def any?
+      result = false
+      each { |e| result = true if yield e }
+    result
+    end
+
+end
